163b29b766bb000116987a49a14b9ce3eb1e8fcf
Voob <vansh.singh8272@gmail.com>
2025-02-04 17:42:20 +0530

    Peter Parker have a small penis

--- DBMS/Test.sql
+++ DBMS/Test.sql
@@ -1,4 +1,8 @@
-CLEAR SCREEN
+
+CLEAR SCREEN;
+
+DELETE FROM student_table 
+WHERE std_id IN (SELECT std_id FROM student_table WHERE class_id=12);
 
 -- SELECT teach_id, class_id FROM subjects_table;
 -- SELECT table_name FROM user_tables;
