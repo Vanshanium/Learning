6773d9179795df11d11416f3e4266cb1f35c3f10
Voob <vansh.singh8272@gmail.com>
2025-02-06 17:22:23 +0530

    Hell yeah

--- DBMS/Test.sql
+++ DBMS/Test.sql
@@ -1,8 +1,8 @@
+-- CLEAR SCREEN;
 
-CLEAR SCREEN;
+ALTER TABLE student_table 
+RENAME student_table TO students_table;
 
-DELETE FROM student_table 
-WHERE std_id IN (SELECT std_id FROM student_table WHERE class_id=12);
 
 -- SELECT teach_id, class_id FROM subjects_table;
 -- SELECT table_name FROM user_tables;
